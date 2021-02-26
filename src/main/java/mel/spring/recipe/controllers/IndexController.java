bd803851921488ecad9a982652230a85d9a64bd4
Melike Bakır <39765068+MelikeBkr@users.noreply.github.com>
2021-02-26 14:08:31 +0000

    Argument Capture usage was tried

--- src/main/java/mel/spring/recipe/controllers/IndexController.java
+++ src/main/java/mel/spring/recipe/controllers/IndexController.java
@@ -1,16 +1,10 @@
 package mel.spring.recipe.controllers;
 
 import lombok.extern.slf4j.Slf4j;
-import mel.spring.recipe.domain.Category;
-import mel.spring.recipe.domain.UnitOfMeasure;
-import mel.spring.recipe.repositories.CategoryRepository;
-import mel.spring.recipe.repositories.UnitOfMeasureRepository;
 import mel.spring.recipe.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 @Slf4j
 @Controller
