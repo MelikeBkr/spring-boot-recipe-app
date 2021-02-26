2b1ec3911e29a65a1aae86f0ac0d308f1759ec17
Melike Bakır <39765068+MelikeBkr@users.noreply.github.com>
2021-02-26 12:24:14 +0000

    Mockito mocks were used

---
+++ src/test/java/mel/spring/recipe/services/RecipeServiceImplTest.java
@@ -0,0 +1,40 @@
+package mel.spring.recipe.services;
+
+import mel.spring.recipe.domain.Recipe;
+import mel.spring.recipe.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class RecipeServiceImplTest
+{
+    RecipeServiceImpl recipeService;
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+    @Test
+    public void getRecipes() throws Exception
+    {
+        Recipe recipe = new Recipe();
+        HashSet<Recipe> recipeSet = new HashSet<>();
+        recipeSet.add(recipe);
+        when(recipeRepository.findAll()).thenReturn(recipeSet);
+        Set<Recipe> recipes = recipeService.getRecipes();
+        assertEquals(1,recipes.size());
+        verify(recipeRepository,times(1)).findAll();
+    }
+}
