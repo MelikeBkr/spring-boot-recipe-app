359c3b4a39de63108997b46a9bab9b5b812109e9
Melike Bakır <39765068+MelikeBkr@users.noreply.github.com>
2021-03-02 17:34:09 +0000

    Recipe details page (show.html) was updated to make getRecipesTest in RecipeControllerTest pass.

---
+++ src/main/java/mel/spring/recipe/SpringBootRecipeAppApplication.java
@@ -0,0 +1,13 @@
+package mel.spring.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRecipeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRecipeAppApplication.class, args);
+	}
+
+}
