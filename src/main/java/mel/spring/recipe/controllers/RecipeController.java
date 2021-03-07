6e6495109e785d844cc33af1e20e6c69101cc57e
Melike Bakır <39765068+MelikeBkr@users.noreply.github.com>
2021-03-07 17:22:48 +0000

    Problem in View and Show Ingredients actions are fixed. Closes #1

--- src/main/java/mel/spring/recipe/controllers/RecipeController.java
+++ src/main/java/mel/spring/recipe/controllers/RecipeController.java
@@ -9,25 +9,31 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @Controller
-public class RecipeController
-{
+public class RecipeController {
+
     private final RecipeService recipeService;
 
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
     }
 
+    @GetMapping
     @RequestMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model){
+
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
     }
+
+    @GetMapping
     @RequestMapping("recipe/new")
     public String newRecipe(Model model){
         model.addAttribute("recipe", new RecipeCommand());
 
         return "recipe/recipeform";
     }
+
+    @GetMapping
     @RequestMapping("recipe/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
@@ -40,6 +46,7 @@ public class RecipeController
 
         return "redirect:/recipe/" + savedCommand.getId() + "/show";
     }
+
     @GetMapping
     @RequestMapping("recipe/{id}/delete")
     public String deleteById(@PathVariable String id){
