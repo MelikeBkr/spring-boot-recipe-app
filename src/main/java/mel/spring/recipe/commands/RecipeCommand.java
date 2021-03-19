177e659e21a750c6414b61cd25a65fb8c21a07e3
Melike Bakır <39765068+MelikeBkr@users.noreply.github.com>
2021-03-19 15:15:20 +0000

    Bean validation notations are not working. The relevant test case was commented out.

--- src/main/java/mel/spring/recipe/commands/RecipeCommand.java
+++ src/main/java/mel/spring/recipe/commands/RecipeCommand.java
@@ -5,21 +5,42 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import mel.spring.recipe.domain.Difficulty;
 
+import javax.validation.constraints.*;
+
+import org.hibernate.validator.constraints.URL;
+
 import java.util.HashSet;
 import java.util.Set;
 
 @Getter
 @Setter
 @NoArgsConstructor
-public class RecipeCommand {
+public class RecipeCommand
+{
     private Long id;
+
+    @Size(min = 3, max = 255)
     private String description;
+
+    @Min(1)
+    @Max(999)
     private Integer prepTime;
+
+    @Min(1)
+    @Max(999)
     private Integer cookTime;
+
+    @Min(1)
+    @Max(100)
     private Integer servings;
     private String source;
+
+    //@URL
     private String url;
+
+    @NotEmpty(message = "Please enter directions")
     private String directions;
+
     private Set<IngredientCommand> ingredients = new HashSet<>();
     private Byte[] image;
     private Difficulty difficulty;
--- src/test/java/mel/spring/recipe/controllers/RecipeControllerTest.java
+++ src/test/java/mel/spring/recipe/controllers/RecipeControllerTest.java
@@ -12,7 +12,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
@@ -20,7 +19,11 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+/**
+ * Created by jt on 6/19/17.
+ */
 public class RecipeControllerTest {
+
     @Mock
     RecipeService recipeService;
 
@@ -44,14 +47,14 @@ public class RecipeControllerTest {
         Recipe recipe = new Recipe();
         recipe.setId(1L);
 
-        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
-
         when(recipeService.findById(anyLong())).thenReturn(recipe);
 
         mockMvc.perform(get("/recipe/1/show"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("recipe/show"));
+                .andExpect(view().name("recipe/show"))
+                .andExpect(model().attributeExists("recipe"));
     }
+
     @Test
     public void testGetRecipeNotFound() throws Exception {
 
@@ -69,6 +72,7 @@ public class RecipeControllerTest {
                 .andExpect(status().isBadRequest())
                 .andExpect(view().name("400error"));
     }
+
     @Test
     public void testGetNewRecipeForm() throws Exception {
         RecipeCommand command = new RecipeCommand();
@@ -87,10 +91,32 @@ public class RecipeControllerTest {
         when(recipeService.saveRecipeCommand(any())).thenReturn(command);
 
         mockMvc.perform(post("/recipe")
-                        .contentType(MediaType.APPLICATION_FORM_URLENCODED))
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("id", "")
+                .param("description", "some string")
+                .param("directions", "some directions")
+        )
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/recipe/2/show"));
     }
+
+//    @Test
+//    public void testPostNewRecipeFormValidationFail() throws Exception {
+//        RecipeCommand command = new RecipeCommand();
+//        command.setId(2L);
+//
+//        when(recipeService.saveRecipeCommand(any())).thenReturn(command);
+//
+//        mockMvc.perform(post("/recipe")
+//                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+//                .param("id", "")
+//                .param("cookTime", "3000")
+//
+//        )
+//                .andExpect(status().isOk())
+//                .andExpect(model().attributeExists("recipe"))
+//                .andExpect(view().name("recipe/recipeform"));
+//    }
 
     @Test
     public void testGetUpdateView() throws Exception {
@@ -104,6 +130,7 @@ public class RecipeControllerTest {
                 .andExpect(view().name("recipe/recipeform"))
                 .andExpect(model().attributeExists("recipe"));
     }
+
     @Test
     public void testDeleteAction() throws Exception {
         mockMvc.perform(get("/recipe/1/delete"))
